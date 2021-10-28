12dc9cc5f0c5f9056c0f14468b900ae45ea6d433
statistical-group-testing <84459228+statistical-group-testing@users.noreply.github.com>
2021-10-28 17:54:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ THRESHOLD_NORMAL_DIST = 0.05
 THRESHOLD_INDEPENDENT_TEST = 0.05
 
 ROOT_DIR = Path(__file__).parent
-
+ 
 
 RESULTS_OUTPUT_PATH = os.path.join(ROOT_DIR, "experiments/results")
 TIME_OUTPUT_PATH = os.path.join(ROOT_DIR, "experiments/time")
